07e75e72f550440a202451785783ee5610462dd9
anggawanridho <55708748+anggawanridho@users.noreply.github.com>
2020-10-21 10:52:29 +0700

    Add files via upload
    first commit

---
+++ src/latihan9teks/Latihan9Teks.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan9teks;
+
+/**
+ *
+ * @author
+ * NAMA     : Muhammad Anggawan Ridho Islami
+ * KELAS    : IF 10K
+ * NIM      : 10119909
+ */
+public class Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        
+        c = 'A';
+        System.out.println("Hasil " + newline + "Char: " + tab + "berisi " + c );
+    }
+    
+}
